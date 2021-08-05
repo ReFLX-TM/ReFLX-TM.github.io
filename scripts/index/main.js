2e50ecd213573de4f26ef864b2f66566aa81b045
Ricardo Hernández <ricardoahe12@gmail>
2021-08-05 10:52:41 -0500

    Arreglando sistema de localStorage

--- scripts/index/main.js
+++ scripts/index/main.js
@@ -2,3 +2,14 @@ import * as nocturno from "./nocturno.js";
 import * as hamburguesa from "./hamburguesa.js";
 import * as trendingGif from "./trendingGif.js";
 import * as busqueda from "./busqueda.js";
+
+const storage = window.localStorage;
+let modoNocturno = false
+let favoritos = [];
+let creados = []
+
+if (storage.length < 3){
+    storage.setItem("favoritos", JSON.stringify(favoritos));
+    storage.setItem("creados", JSON.stringify(creados));
+    storage.setItem("nocturno", JSON.stringify(modoNocturno));
+}
